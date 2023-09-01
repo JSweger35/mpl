28dcb241b7ddb1249cff1f26eac9247985fa48f8
Jake S <jsweg13@gmail.com>
2023-09-01 12:41:42 -0400

    Add labels and title

--- mpl_squares.py
+++ mpl_squares.py
@@ -4,5 +4,12 @@ squares = [1, 4, 9, 16, 25]
 fig, ax = plt.subplots()
 ax.plot(squares, linewidth=3)
 
+# Set chart title and label axes.
+ax.set_title("Square nubmers", fontsize=24)
+ax.set_xlabel("Value", fontsize=14)
+ax.set_ylabel("Square of Value", fontsize=14)
+
+
+
 
 plt.show()
