1c9f1aa30101da93faacd7d26e7640b42fd26c30
Jake S <jsweg13@gmail.com>
2023-09-01 12:43:07 -0400

    Update tick marks

--- mpl_squares.py
+++ mpl_squares.py
@@ -9,7 +9,8 @@ ax.set_title("Square numbers", fontsize=24)
 ax.set_xlabel("Value", fontsize=14)
 ax.set_ylabel("Square of Value", fontsize=14)
 
-
+# Set size of tick labels.
+ax.tick_params(axis='both', labelsize=14)
 
 
 plt.show()
