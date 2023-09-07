3774ed3151a5d07c0f02e5758ab3ec40ffd2be15
Jake S <jsweg13@gmail.com>
2023-09-07 12:30:51 -0400

    set size of tick labels

--- scatter_squares.py
+++ scatter_squares.py
@@ -9,4 +9,7 @@ ax.set_title("Square Numbers", fontsize=24)
 ax.set_xlabel("Value", fontsize=24)
 ax.set_ylabel("Square of Value", fontsize=24)
 
+# Set size of tick labels.
+ax.tick_params(axis='both', which='major', labelsize='14')
+
 plt.show()
