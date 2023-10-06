64c0165518f8002033aabb06aa424c02b707d153
Jake S <jsweg13@gmail.com>
2023-10-05 21:08:11 -0400

    add continue for no movement

--- random_walk.py
+++ random_walk.py
@@ -26,5 +26,7 @@ class RandomWalk:
             y_distance = choice([0, 1, 2, 3, 4])
             y_step = y_direction * y_distance
             
-            
+            # Reject moves that go nowhere
+            if x_step == 0 and y_step == 0:
+                continue
             
