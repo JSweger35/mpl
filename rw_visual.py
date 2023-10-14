6db842e9eefcbc5c24a707f5e0111e5bcbe2d162
Jake S <jsweg13@gmail.com>
2023-10-13 20:14:31 -0400

    Remove duplicate

--- rw_visual.py
+++ rw_visual.py
@@ -13,7 +13,6 @@ while True:
     fig, ax = plt.subplots()
     point_numbers = range(rw.num_points)
     ax.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues, edgecolors='none', s=15)
-    ax.scatter(rw.x_values, rw.y_values, s=15)
     plt.show()
     
     keep_running = input("Make another walk? (y/n): ")
