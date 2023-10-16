9ba96caacff50afab4ca5e0a4d5750ebb1a55249
Jake S <jsweg13@gmail.com>
2023-10-16 12:22:31 -0400

    decrease point size

--- rw_visual.py
+++ rw_visual.py
@@ -12,7 +12,7 @@ while True:
     plt.style.use('classic')
     fig, ax = plt.subplots()
     point_numbers = range(rw.num_points)
-    ax.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues, edgecolors='none', s=15)
+    ax.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues, edgecolors='none', s=1)
     
     # Emphasize the first and last points.
     ax.scatter(0, 0, c='green', edgecolors='none', s=100)
