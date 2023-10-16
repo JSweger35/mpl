9450ea3a3a166187aecae77bb4c7298de33fd631
Jake S <jsweg13@gmail.com>
2023-10-16 12:19:47 -0400

    remove x axis

--- rw_visual.py
+++ rw_visual.py
@@ -18,6 +18,9 @@ while True:
     ax.scatter(0, 0, c='green', edgecolors='none', s=100)
     ax.scatter(rw.x_values[-1], rw.y_values[-1], c='red', edgecolors='none', s=100)
     
+    # Remove the axis
+    ax.get_xaxis().set_visible(False)
+    
     plt.show()
     
     keep_running = input("Make another walk? (y/n): ")
