3489bed1dfad186a2f4041d52c8518bac81631eb
Jake S <jsweg13@gmail.com>
2023-10-18 12:03:51 -0400

    declare num_sides

--- die.py
+++ die.py
@@ -2,3 +2,9 @@ from random import randint
 
 class Die:
     """A class representing a single die"""
+    
+    def __init__(self, num_side=6):
+        "Assume a six-sided die"
+        self.num_sides = num_sides
+        
+    
