48f3e8eec772732f0014b180e1109c6e11746ecd
Jake S <jsweg13@gmail.com>
2023-10-18 12:22:04 -0400

    increase range size

--- die_visual.py
+++ die_visual.py
@@ -5,7 +5,7 @@ die = Die()
 
 # Make some rolls, and store results in a list
 results = []
-for roll in range(100):
+for roll in range(1000):
     result = die.roll()
     results.append(result)
     
