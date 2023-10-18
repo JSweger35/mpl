2dbb8accab6fbf048bc39b6862299789dcdbc9b1
Jake S <jsweg13@gmail.com>
2023-10-18 12:25:03 -0400

    fix typo

--- die_visual.py
+++ die_visual.py
@@ -10,7 +10,7 @@ for roll in range(1000):
     results.append(result)
     
 # Analyze results
-Frequencies = []
+frequencies = []
 for value in range(1, die.num_sides+1):
     frequency = results.count(value)
     frequencies.append(frequency)
