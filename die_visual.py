da9c77c2816b0bcfdf954a366ecec6114658ae14
Jake S <jsweg13@gmail.com>
2023-10-19 12:25:30 -0400

    change values to axis

--- die_visual.py
+++ die_visual.py
@@ -1,4 +1,4 @@
-from plotly import Bar, Layout
+from plotly.graph_objs import Bar, Layout
 from plotly import offline
 
 from die import Die
@@ -22,8 +22,8 @@ for value in range(1, die.num_sides+1):
 x_values = list(range(1, die.num_sides+1))
 data = [Bar(x=x_values, y=frequencies)]
 
-x_values_config = {'title': 'Result'}
-y_values_config = {'title': 'Frequency of Result'}
+x_axis_config = {'title': 'Result'}
+y_axis_config = {'title': 'Frequency of Result'}
 my_layout = Layout(title='Results of rolling one D6 1000 times', xaxis=x_axis_config, yaxis=y_axis_config)
 offline.plot({'data':data, 'layout':my_layout}, filename='d6.html')
     
