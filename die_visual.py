4f1aa9d5b0e4e5a5e3f004ec78f49e7d228cd21f
Jake S <jsweg13@gmail.com>
2023-10-20 12:25:16 -0400

    Update range lenght

--- die_visual.py
+++ die_visual.py
@@ -9,7 +9,7 @@ die_2 = Die(10)
 
 # Make some rolls, and store results in a list
 results = []
-for roll in range(1000):
+for roll_num in range(50000):
     result = die_1.roll() + die_2.roll()
     results.append(result)
     
