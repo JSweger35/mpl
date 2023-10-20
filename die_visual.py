013490db5f996e6c0ad0f73343c3714614f1c423
Jake S <jsweg13@gmail.com>
2023-10-20 12:25:59 -0400

    Update file naming

--- die_visual.py
+++ die_visual.py
@@ -26,8 +26,8 @@ data = [Bar(x=x_values, y=frequencies)]
 
 x_axis_config = {'title': 'Result', 'dtick': 1}
 y_axis_config = {'title': 'Frequency of Result'}
-my_layout = Layout(title='Results of rolling two D6 1000 times', xaxis=x_axis_config, yaxis=y_axis_config)
-offline.plot({'data':data, 'layout':my_layout}, filename='d6_d6.html')
+my_layout = Layout(title='Results of rolling one D6 and one D10 50000 times', xaxis=x_axis_config, yaxis=y_axis_config)
+offline.plot({'data':data, 'layout':my_layout}, filename='d6_10.html')
     
     
 print(frequencies)
