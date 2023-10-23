cce5da54b229d5e1152dabf373de5e247d900607
Jake S <jsweg13@gmail.com>
2023-10-23 12:28:56 -0400

    make comment

--- sitka_highs.py
+++ sitka_highs.py
@@ -5,8 +5,8 @@ with open(filename) as f:
     reader = csv.reader(f)
     header_row = next(reader)
     
-    for index, column_header in enumerate(header_row):
-        print(index, column_header)
+    # for index, column_header in enumerate(header_row):
+        # print(index, column_header)
         
     # Get high temperatures from this file
     highs = []
