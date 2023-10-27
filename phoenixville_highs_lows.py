86894836c7cf04400c70f7c5f2d72ae15ffc2bad
Jake S <jsweg13@gmail.com>
2023-10-27 12:17:06 -0400

    fix typo

--- phoenixville_highs_lows.py
+++ phoenixville_highs_lows.py
@@ -8,5 +8,5 @@ with open(filename) as f:
     reader = csv.reader(f)
     header_row = next(reader)
     
-    for index, column_header in ennumerate(header_row):
+    for index, column_header in enumerate(header_row):
         print(index, column_header)
