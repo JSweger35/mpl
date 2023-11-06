7926a82e6cc6f460ddf5cf638229c204d5831219
Jake S <jsweg13@gmail.com>
2023-11-06 12:19:02 -0500

    add file

--- eq_explore_data.py
+++ eq_explore_data.py
@@ -25,6 +25,7 @@ data = [Scattergeo(lon=lons, lat=lats)]
 my_layout = Layout(title='Global Earthquakes')
 
 fig = {'data': data, 'layout': my_layout}
+offline.plot(fig, filename='global_earthquake.html')
 print(mags[:10])
 print(lons[:5])
 print(lats[:5])
