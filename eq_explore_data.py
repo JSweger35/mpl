cfacf03b3c69e314eab170a9745de65b7ac03930
Jake S <jsweg13@gmail.com>
2023-11-13 12:17:11 -0500

    add title varaiable from properties

--- eq_explore_data.py
+++ eq_explore_data.py
@@ -16,6 +16,7 @@ for eq_dict in all_eq_dicts:
     mag = eq_dict['properties']['mag']
     lon = eq_dict['geometry']['coordinates'][0]
     lat = eq_dict['geometry']['coordinates'][1]
+    title = eq_dict['properties']['title']
     mags.append(mag)
     lons.append(lon)
     lats.append(lat)
